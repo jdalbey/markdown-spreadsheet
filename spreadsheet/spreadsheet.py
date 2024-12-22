a30945c5f396e17514f83129273f1ac39f041bb3
John Dalbey <jdalbey@calpoly.edu>
2024-12-21 17:21:48 -0800

    Add a local main for unit testing.

--- spreadsheet/spreadsheet.py
+++ spreadsheet/spreadsheet.py
@@ -262,3 +262,14 @@ def pretty_print_results(result):
     table = tabulate(rows, headers="keys")
 
     print(table)
+
+# A local main for unit testing
+if __name__ == '__main__':
+
+    spreadsheet = {"A1": "1", "A2": "94", "A3": "2", "B1": "", "B4": "=SUM(A1:A2)", "D1": "1", "F1": "=A2"}
+
+    result = evaluate_spreadsheet(spreadsheet)
+
+    print(result)
+
+    pretty_print_results(result)
