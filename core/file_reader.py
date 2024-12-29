dd249aa8ceb33b6d0c934c362b0a90586e26c6b4
John Dalbey <jdalbey@calpoly.edu>
2024-12-28 19:49:55 -0800

    Changed interface to file_reader.py to accommodate PyQT conversion.

--- core/file_reader.py
+++ core/file_reader.py
@@ -1,11 +1,11 @@
 
 class FileReader:
     def read_file(self, file_path: str) -> list:
-        # Logic to read and parse the file
+        # Logic to read the file
         if file_path:
             try:
                 with open(file_path, "r") as file:
-                    return file.readlines()
+                    return file.read()
             except Exception as e:
                 print ("Error", f"Failed to open file: {e}")
 
