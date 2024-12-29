6bd8ac042ba97c811881ad66c11a0c15a5e7a20b
John Dalbey <jdalbey@calpoly.edu>
2024-12-28 19:43:32 -0800

    rename test_controller to test_app_controller.py

--- tests/test_app_controller.py
+++ tests/test_app_controller.py
@@ -1,8 +1,9 @@
-import unittest
-import app_controller
-import tempfile
 
-class TestController(unittest.TestCase):
+import tempfile
+import app_controller
+
+
+class TestController:
 
     def create_temp_file(self, content, extension):
         """Helper method to create a temporary file with specific content and extension."""
@@ -14,17 +15,18 @@ class TestController(unittest.TestCase):
     def test_controller(self):
         ct = app_controller.AppController()
         content = "| Peaches | Pears | Total |\n|-------|-----|\n| 2  | 3  | =A1+B1\n"
-        tempfilename = self.create_temp_file(content,"md")
+        tempfilename = self.create_temp_file(content, "md")
+
         # Read input file
         content = ct.read_file_content(tempfilename)
-        # send to recalculate
-        worksheet = ct.evaluate(content)
-        # get results from model and write to HTML file using a table for the grid.
-        outfilename = self.create_temp_file("<HTML>", "html")
-        ct.writer.write(worksheet,outfilename,"HTML")
-        # Read the result back in so we can test it
-        actualhtml = ct.read_file_content(outfilename)[0]
-        self.assertEqual("<HTML><TABLE><TR><td>2",actualhtml[:22])
 
-if __name__ == "__main__":
-    unittest.main()
+        # Send to recalculate
+        worksheet = ct.evaluate(content.splitlines())
+
+        # Get results from model and write to HTML file using a table for the grid
+        outfilename = self.create_temp_file("<HTML>", "html")
+        ct.writer.write(worksheet, outfilename, "HTML")
+
+        # Read the result back in so we can test it
+        actualhtml = ct.read_file_content(outfilename)
+        assert actualhtml[:22] == "<HTML><TABLE><TR><td>2"
